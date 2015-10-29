b20bb52e6accaf6e3a9c6db8fc6acd9b264ff4b4
Johannes Kronmüller <Johannes.Kronmueller@gmail.com>
2015-10-29 21:16:10 +0100

    Can now create Databases und Database Users & implemented Logging

--- src/de/amazingcode/Bot.java
+++ src/de/amazingcode/Bot.java
@@ -1,24 +1,26 @@
 /**
  * @author http://www.amazingcode.de
- * @version 1.0.1
- * created on 2015-10-28
+ * @version 1.1
+ * created on 2015-10-29
  */
 
 package de.amazingcode;
 
-import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.logging.Logger;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.client.utils.URIUtils;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
@@ -29,46 +31,42 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Bot {
+	private final static Logger LOGGER = Logger.getLogger(Bot.class.getName());
 	private CookieStore cookieStore;
 	private RequestConfig requestConfig;
 	private CloseableHttpClient httpclient;
-	
+
 	private final String host;
 	private final String username;
 	private final String password;
-	
 	private final String useragent;
-	
+
+	private String userPath;
+
 	public Bot(String host, String username, String password) {
 		this.host = host;
 		this.username = username;
 		this.password = password;
-		
-		this.useragent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0";
-		
-		Registry<CookieSpecProvider> r = RegistryBuilder.<CookieSpecProvider>create()
-		        .register("easy", new EasySpecProvider())
-		        .build();
-		
-		cookieStore = new BasicCookieStore();
-		
-		requestConfig = RequestConfig.custom()
-				.setCookieSpec("easy")
-				.build();
 
-		httpclient = HttpClients.custom()
-				.setUserAgent(this.useragent)
-				.setDefaultCookieStore(cookieStore)
-				.setRedirectStrategy(new LaxRedirectStrategy())
-				.setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-				.setDefaultCookieSpecRegistry(r)
-				.setDefaultRequestConfig(requestConfig)
-				.build();
+		this.useragent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0";
+
+		Registry<CookieSpecProvider> r = RegistryBuilder.<CookieSpecProvider> create()
+				.register("easy", new EasySpecProvider()).build();
+
+		cookieStore = new BasicCookieStore();
+
+		requestConfig = RequestConfig.custom().setCookieSpec("easy").build();
+
+		httpclient = HttpClients.custom().setUserAgent(this.useragent).setDefaultCookieStore(cookieStore)
+				.setRedirectStrategy(new LaxRedirectStrategy()).setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+				.setDefaultCookieSpecRegistry(r).setDefaultRequestConfig(requestConfig).build();
 	}
 
-	public boolean login() throws ClientProtocolException, IOException {
+	public void login() throws Exception {
 		// get initial cookies
 		HttpGet httpGet = new HttpGet(this.host);
 		CloseableHttpResponse response1 = httpclient.execute(httpGet);
@@ -78,7 +76,7 @@ public class Bot {
 		} finally {
 			response1.close();
 		}
-		
+
 		// actual login
 		HttpPost httpPost = new HttpPost(this.host + "login/");
 		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
@@ -88,21 +86,83 @@ public class Bot {
 		httpPost.setEntity(new UrlEncodedFormEntity(nvps));
 		httpPost.addHeader("Referer", this.host);
 
-		CloseableHttpResponse response2 = httpclient.execute(httpPost);
-	
+		HttpClientContext context = HttpClientContext.create();
+		CloseableHttpResponse response2 = httpclient.execute(httpPost, context);
+
 		try {
-			int statusCode = response2.getStatusLine().getStatusCode();
-			HttpEntity entity2 = response2.getEntity();
-			EntityUtils.consume(entity2);
-			return (statusCode == 200);
+			if (response2.getStatusLine().getStatusCode() != 200) {
+				throw new Exception("Login failed.");
+			}
+
+			// extract user path
+			HttpHost target = context.getTargetHost();
+			List<URI> redirectLocations = context.getRedirectLocations();
+			String location = URIUtils.resolve(httpPost.getURI(), target, redirectLocations).toString();
+			int start = location.indexOf("/", this.host.length() - 1) + 1;
+			int end = location.indexOf("/", start) + 1;
+			this.userPath = location.substring(start, end);
+			LOGGER.info("Logged in as " + this.username + " successfully.");
 		} finally {
 			response2.close();
 		}
 	}
+
+	public void createDatabase(String dbName) throws Exception {
+		HttpPost httpPost = new HttpPost(this.host + this.userPath + "frontend/paper_lantern/sql/addb.html");
+		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+		nvps.add(new BasicNameValuePair("db", dbName));
+		httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+		httpPost.addHeader("Referer", this.host);
+
+		CloseableHttpResponse response = httpclient.execute(httpPost);
+
+		try {
+			if (response.getStatusLine().getStatusCode() != 200) {
+				throw new Exception("Database Creation failed.");
+			}
+			HttpEntity entity = response.getEntity();
+			String html = EntityUtils.toString(entity);
+			if (html.indexOf("wurde hinzugefügt") < 0) {
+				Document doc = Jsoup.parse(html);
+				String errorMessage = doc.getElementById("addbErrorMsg").text();
+				throw new Exception("Database Creation failed. (" + errorMessage + ")");
+			}
+			EntityUtils.consume(entity);
+			LOGGER.info("Database " + dbName + " created successfully.");
+
+		} finally {
+			response.close();
+		}
+	}
 	
-	//TODO: implement whatever you want to achieve
-	public void doStuff() {
-		System.out.println("NOT YET IMPLEMENTED.");
+	public void createDatabaseUser(String dbUser, String dbPassword) throws Exception {
+		HttpPost httpPost = new HttpPost(this.host + this.userPath + "frontend/paper_lantern/sql/adduser.html");
+		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+		nvps.add(new BasicNameValuePair("pass", dbPassword));
+		nvps.add(new BasicNameValuePair("pass2", dbPassword));
+		nvps.add(new BasicNameValuePair("user", dbUser));
+		httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+		httpPost.addHeader("Referer", this.host);
+
+		CloseableHttpResponse response = httpclient.execute(httpPost);
+
+		try {
+			if (response.getStatusLine().getStatusCode() != 200) {
+				throw new Exception("Database User Creation failed.");
+			}
+			HttpEntity entity = response.getEntity();
+			String html = EntityUtils.toString(entity);
+			if (html.indexOf("erfolgreich") < 0) {
+				Document doc = Jsoup.parse(html);
+				String errorMessage = doc.getElementById("adduserErrorMsg").text();
+				throw new Exception("Database User Creation failed. (" + errorMessage + ")");
+			}
+			EntityUtils.consume(entity);
+			LOGGER.info("Database User " + dbUser + " created successfully.");
+
+		} finally {
+			response.close();
+		}
 	}
 
 }
--- src/de/amazingcode/Workflow.java
+++ src/de/amazingcode/Workflow.java
@@ -1,32 +1,47 @@
 /**
  * @author http://www.amazingcode.de
- * @version 1.0
- * created on 2015-10-27
+ * @version 1.1
+ * created on 2015-10-29
  */
 
 package de.amazingcode;
 
-import java.io.IOException;
-
-import org.apache.http.client.ClientProtocolException;
+import java.util.logging.Logger;
 
 public class Workflow {
+	private final static Logger LOGGER = Logger.getLogger(Bot.class.getName());
 	/**
 	 * with protocol, port and trailing slash
 	 * example: http://x3demoa.cpx3demo.com:2082/
 	 */
-	private static final String host = "http://x3demoa.cpx3demo.com:2082/";
-	private static final String username = "x3demoa";
-	private static final String password = "x3demoa";
+	private static final String host = "";
+	private static final String username = "";
+	private static final String password = "";
+	
+	private static final String dbName = "";
+	private static final String dbUser = "";
+	private static final String dbPassword = "";
 
-	public static void main(String args[]) throws ClientProtocolException, IOException {
+	public static void main(String args[]) throws Exception {
 		Bot bot = new Bot(host, username, password);
 		
-		if(bot.login()) {
-			System.out.println("Login successful!");
+		try {
+			bot.login();
+		} catch(Exception e) {
+			LOGGER.severe(e.getMessage());
+			System.exit(-1);
 		}
 		
-		// use bot to automate some stuff in cPanel...
-		bot.doStuff();
+		try {
+			bot.createDatabase(dbName);
+		} catch(Exception e) {
+			LOGGER.severe(e.getMessage());
+		}
+		
+		try {
+			bot.createDatabaseUser(dbUser, dbPassword);
+		} catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
 }
